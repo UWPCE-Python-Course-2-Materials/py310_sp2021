717c63e63aa34cbd92312adaeac9e9bbcc18ce7f
andykmiles <akmiles@icloud.com>
2021-04-28 10:43:01 -0700

    more lesson4 material added

--- src/py310/lesson04/unit_test_ex.py
+++ src/py310/lesson04/unit_test_ex.py
@@ -14,5 +14,19 @@ class Testing(unittest.TestCase):
         self.assertEqual(a, b)
 
 
+# pytest only needs from here...
+def test_string_pytest():
+    a = 'some'
+    b = 'some'
+    assert a == b
+
+
+def test_boolean(self):
+    a = True
+    b = True
+    assert a is b
+# to here
+
+
 if __name__ == '__main__':
     unittest.main()
